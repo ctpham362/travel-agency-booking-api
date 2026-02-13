6edfa7e45277979677fdcddf4c7c1592d18007b9
Meatbuns <phamnothanh362@gmail.com>
2026-02-12 22:34:49 -0800

    Initial Commit

---
+++ src/main/java/com/example/chithanhd288/ChiThanhD288Application.java
@@ -0,0 +1,13 @@
+package com.example.chithanhd288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiThanhD288Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiThanhD288Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chithanhd288/ChiThanhD288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chithanhd288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiThanhD288ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
