a980479c7efe438f2c52fb84c7e796522fb2bfed
Meatbuns <phamnothanh362@gmail.com>
2026-02-13 20:12:13 -0800

    Completed Task E

--- src/main/java/com/example/chithanhd288/dao/ExcursionRepository.java
+++ src/main/java/com/example/chithanhd288/dao/ExcursionRepository.java
@@ -1,9 +1,9 @@
 package com.example.chithanhd288.dao;
 
-import com.example.chithanhd288.entities.Country;
+import com.example.chithanhd288.entities.Excursion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
-public interface CountryRepository extends JpaRepository<Country, Long> {
+public interface ExcursionRepository extends JpaRepository<Excursion, Long> {
 }
