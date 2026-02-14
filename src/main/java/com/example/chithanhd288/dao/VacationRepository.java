993b45ebf6f51f6064579f436a1db841581b7c36
Chi Thanh Pham <cpham39@wgu.edu>
2026-02-14 10:11:03 +0000

    Revert "Completed Task E"
    This reverts commit a980479c7efe438f2c52fb84c7e796522fb2bfed

--- src/main/java/com/example/chithanhd288/dao/VacationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.chithanhd288.dao;
-
-import com.example.chithanhd288.entities.Vacation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin("http://localhost:4200")
-public interface VacationRepository extends JpaRepository<Vacation, Long> {
-}
